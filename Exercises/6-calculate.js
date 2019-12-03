993b6680ab7aea42e13d95d1667f1ef30c335155
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 17:31:42 +0200

    Update 6-calculate.js

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const square = null;
+const square = (x) => (x ** 2);
 
-const cube = null;
+const cube = (x) => (x ** 3);
 
-const average = null;
+const average = (a, b) => ((a + b) / 2.0);
 
-const calculate = null;
+const calculate = () => {let a = []; for (let i = 0; i <= 9; i++) {a.push(average(square(i), cube(i)))} return a;};
+
+const a = calculate();
+console.log(a);
 
 module.exports = { square, cube, average, calculate };
