4fd41e1aea480f96eda2189fc2300ee4b983ef68
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 17:15:29 +0200

    Update 5-range-odd.js

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const rangeOdd = null;
+const range = (b, e) => {let a = []; for(let i = b; i <= e - 2; i += 2) {a.push(i + 1);} return a;};
+
+const a = range(0, 100);
+console.log(a);
 
 module.exports = { rangeOdd };
