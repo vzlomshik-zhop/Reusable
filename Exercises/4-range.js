b8115b04889eec3e664a0db2dca999e3243dc625
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 17:11:02 +0200

    Update 4-range.js

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const range = null;
+const range = (b, e) => {let a = []; for(let i = b; i <= e; i++) {a.push(i);} return a;};
+
+const a = range(0, 100);
+console.log(a);
 
 module.exports = { range };
