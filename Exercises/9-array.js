892b970ce86371e90aaa8b40f4e952880ca10f53
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 17:47:59 +0200

    Update 9-array.js

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -2,6 +2,6 @@
 
 const phonebook = null;
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => {for (let n of phonebook) {if (name === n.name) {return n.phone}} return null;};
 
 module.exports = { phonebook, findPhoneByName };
